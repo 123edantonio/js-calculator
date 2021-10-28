3200e68d0c4c93978cea3bb96e4cac79dc247d40
123edantonio <71267975+123edantonio@users.noreply.github.com>
2021-10-28 10:28:20 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Use getElementById to get the two numbers and store them in variables
+var num1 = getElementById(inputNum1);
+var num2 = getElementById(inputNum2);
+var num3 = getElementById(multiplyAnswer);
+// Create a function called multiplyNumbers
+function multiplyNumbers() {
+  num1 * num2 = num3;
+}
+// In the function use getElementById to change the ? to the answer of the equation
+
+// If you get that working, create new forms for the other arithmetic operators
